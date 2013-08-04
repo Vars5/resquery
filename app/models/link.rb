a3c8f500d3eda98de71b7784c05c6be5917bc28b
Pasha Rayan <pasha.rayan@gmail.com>
2013-08-04 22:32:50 +1000

    added model validation for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,11 @@
 class Link < ActiveRecord::Base
   attr_accessible :link, :title, :user_id, :group_id
 
-  #associations
   belongs_to :group
+
+  validates :title, :presence => true
+  validates :link, :presence => true
+
 
 
 end
