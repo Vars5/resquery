d777db2d4f9268b7aff2d5d3c05cb0973653bdc5
Pasha Rayan <pasha.rayan@gmail.com>
2013-08-11 23:21:05 +1000

    changed discussion show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     user = User.find_by_id(user_id)
     full_name = "#{user.first_name} " "#{user.last_name}"
   end
+
+  def discussion_group(group_id)
+    discussion_group = Group.find_by_id(group_id)
+		link_to discussion_group.name, discussion_group
+  end
+
 end
