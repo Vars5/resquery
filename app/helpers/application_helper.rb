f62e990cffa9458b5024c6f8d68ed9ee694bdb82
Pasha Rayan <pasha.rayan@gmail.com>
2013-08-31 15:16:09 +1000

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,18 +10,5 @@ module ApplicationHelper
 		link_to discussion_group.name, discussion_group
   end
   
-  #unsimple format has been used to remove the default <p> that simple_format adds in when displaying text
-  def unsimple_format(text)
-    text.gsub(/\r\n?/, "\n").split(%r{(\n*(?:<h4>.*?</h4>|<blockquote(?: .*?)?>.*?</blockquote>)\n*)}im).map do |part|
-      case part
-      when /\A\s*\Z/, /<h4>/
-        part
-      when /blockquote/
-        part.sub(%r{(<blockquote(?: .*?)?>)(.*?)(</blockquote>)}im) { $1 + simple_format($2) + $3 }
-      else
-        simple_format(part)
-      end
-    end.join
-  end
 
 end
